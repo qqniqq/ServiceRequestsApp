4b5a079a1e7c937875c4b1f086c498765c43845d
69sai <69sai@HUAWEI>
2025-12-24 18:53:00 +0300

    Добавил элементы на форму

---
+++ ServiceRequestsApp/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ServiceRequestsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewRequests = new System.Windows.Forms.DataGridView();
+            this.btnAddRequest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRequests)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewRequests
+            // 
+            this.dataGridViewRequests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRequests.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridViewRequests.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewRequests.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewRequests.Name = "dataGridViewRequests";
+            this.dataGridViewRequests.RowHeadersWidth = 51;
+            this.dataGridViewRequests.RowTemplate.Height = 24;
+            this.dataGridViewRequests.Size = new System.Drawing.Size(1100, 412);
+            this.dataGridViewRequests.TabIndex = 0;
+            // 
+            // btnAddRequest
+            // 
+            this.btnAddRequest.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddRequest.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAddRequest.Location = new System.Drawing.Point(0, 432);
+            this.btnAddRequest.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddRequest.Name = "btnAddRequest";
+            this.btnAddRequest.Size = new System.Drawing.Size(224, 44);
+            this.btnAddRequest.TabIndex = 1;
+            this.btnAddRequest.Text = "Добавить заявку";
+            this.btnAddRequest.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 619);
+            this.Controls.Add(this.btnAddRequest);
+            this.Controls.Add(this.dataGridViewRequests);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "Учет заявок на обслуживание техники";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRequests)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewRequests;
+        private System.Windows.Forms.Button btnAddRequest;
+    }
+}
+
