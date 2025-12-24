34f073e1822b643c9168162b865f360e2f4a9560
69sai <69sai@HUAWEI>
2025-12-24 18:39:51 +0300

    добавил модель заявок

---
+++ ServiceRequestsLib/ServiceRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ServiceRequestsLib
+{
+    public class ServiceRequest
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public string Contact { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ProblemType { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Specialist { get; set; }
+        public string Status { get; set; }
+    }
+}
